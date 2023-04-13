06d1f48d79c9a7341f549226e31449da3e892151
Uliana <uosintseva@gmail.com>
2023-04-13 14:41:16 +0300

    change files

--- src/test/java/ru/netology/TicketManagerTest.java
+++ src/test/java/ru/netology/TicketManagerTest.java
@@ -29,7 +29,7 @@ public class TicketManagerTest {
         manager.add(ticket7);
         manager.add(ticket8);
 
-        Ticket[] expected = {ticket8,ticket1,ticket3, ticket5};
+        Ticket[] expected = {ticket1,ticket3, ticket5};
         Ticket[] actual = manager.findAll("SVO","KZN");
 
         Assertions.assertArrayEquals(expected, actual);
@@ -103,10 +103,10 @@ public class TicketManagerTest {
         TicketRepository repo = new TicketRepository();
         TicketManager manager = new TicketManager(repo);
 
-        Ticket ticket1 = new Ticket(1, "SVO", "KZN",  1299, 130);
+        Ticket ticket1 = new Ticket(1, "SVO", "KZN", 1299, 130);
         Ticket ticket2 = new Ticket(2, "VKO", "LED", 10_475, 135);
         Ticket ticket3 = new Ticket(3, "SVO", "KZN", 5140, 150);
-        Ticket ticket4 = new Ticket(4, "AER", "KZN", 8510 , 135);
+        Ticket ticket4 = new Ticket(4, "AER", "KZN", 8510, 135);
         Ticket ticket5 = new Ticket(5, "SVO", "KZN", 8070, 140);
         Ticket ticket6 = new Ticket(6, "VKO", "SVX", 5199, 115);
         Ticket ticket7 = new Ticket(7, "DME", "KZN", 9320, 155);
@@ -122,12 +122,13 @@ public class TicketManagerTest {
         manager.add(ticket8);
 
 
-        Ticket[] expected = { };
-        Ticket[] actual = manager.findAll("SVX","KZN");
+        Ticket[] expected = {};
+        Ticket[] actual = manager.findAll("SVX", "KZN");
 
         Assertions.assertArrayEquals(expected, actual);
+    }
 
-}
+
 }
 
 
